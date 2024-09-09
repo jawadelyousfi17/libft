e85e7f8715fe4e25198761f577705033ad93d13f
Jawad el yousfi <Jawad el yousfi>
2024-09-09 20:14:01 +0100

    Add ft_atoi and its tests

---
+++ ft_atoi.c
@@ -0,0 +1,43 @@
+int ft_isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return (2048);
+    }
+    return (0);
+}
+
+int ft_isspace(char c)
+{
+    if (c == '\f' || c == '\n' || c == '\t' || c == '\v' || c == '\r' || c == ' ')
+    {
+        return (1);
+    }
+    return (0);
+}
+int ft_atoi(const char *nptr)
+{
+    int result;
+    int sign;
+
+    result = 0;
+    sign = 1;
+    while (*nptr && ft_isspace(*nptr))
+    {
+        nptr++;
+    }
+    if (*nptr == '-' || *nptr == '+')
+    {
+        if (*nptr == '-')
+        {
+            sign = -1;
+        }
+        nptr++;
+    }
+    while (*nptr && ft_isdigit(*nptr))
+    {
+        result = result * 10 + *nptr - 48;
+        nptr++;
+    }
+    return (result * sign);
+}
