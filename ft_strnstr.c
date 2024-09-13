f18766f88ea519aac437f1f6e477b2fb4c3664de
Jawad el yousfi <Jawad el yousfi>
2024-09-13 13:23:18 +0100

    improve ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,33 +1,25 @@
-#include <string.h>
+#include "libft.h"
 
 char *ft_strnstr(const char *big, const char *little, size_t len)
 {
     size_t index;
-    int searching_index;
-    char *tmp_little;
+    size_t little_len;
     
     if (*little == '\0')
     {
         return ((char *)big);
     }
+
+    little_len = ft_strlen(little);
     index = 0;
-    searching_index = 0;
-    tmp_little = (char *)little;
-    while (*big && index < len && *tmp_little)
+    while (*big && index <= len - little_len)
     {
-        searching_index = 0;
-        tmp_little = (char *)little;
-        if (*big == *tmp_little)
-        {
-            while (tmp_little[searching_index] && searching_index + index < len && tmp_little[searching_index] == big[searching_index])
-            {
-                searching_index++;
-            }
-            if (!tmp_little[searching_index])
-                return (char *)big;
-        }
-        big++;
-        index++;
+       if (*big == *little && ft_strncmp(big, little, little_len) == 0)
+       {
+            return ((char*) big);
+       }
+       big++;
+       index++;
     }
     return (0);
 }
